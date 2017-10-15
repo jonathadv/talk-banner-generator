389abf304776f6a1dc25f3ac435c29a2276ecf76
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-10-15 15:27:18 -0200

    Refactoring Event, EventContent and SpeakerList

--- src/event/event-content.js
+++ src/event/event-content.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class EventContent extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            description: 'Lorem Ipsum is a filler text or greeking commonly used to demonstrate the textual elements of a graphic document or visual presentation. Replacing meaningful content with placeholder text allows designers to design the form of the content before the content itself has been produced.'
-        }
-    } 
-  render() {
-    return (
-      <div className="event-content">
-        <p className="event-description" style={{"color" : this.props.color}}>
-            {this.props.description ? this.props.description : this.state.description}
-        </p>
-      </div>
-    );
+    }
+
+    render() {
+
+        const {
+            description,
+            descriptionColor
+        } = this.props;
+
+        const style = {"color" : descriptionColor};
+
+        return (
+            <div className="event-content">
+                <p className="event-description" style={style}>
+                    {description}
+                </p>
+            </div>
+        );
   }
+}
+
+EventContent.propTypes = {
+    description: PropTypes.string,
+    descriptionColor: PropTypes.string,
+}
+
+EventContent.defaultProps = {
+    description: 'Lorem Ipsum is a filler text or greeking commonly used to demonstrate the textual elements of a graphic document or visual presentation. Replacing meaningful content with placeholder text allows designers to design the form of the content before the content itself has been produced.',
+    descriptionColor: "#000000",
 }
 
